f2fc41f252364e5b26422f7242d4beb2eb27e8bc
Pepeluu <jbersan993@g.educaand.es>
2022-01-28 09:50:59 +0100

    Task Form
    Entity, Controller and Form

---
+++ src/Controller/TaskController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\TaskType;
+use App\Entity\Task;
+use Doctrine\Persistence\ManagerRegistry;
+
+class TaskController extends AbstractController
+{
+    /**
+     * @Route("/tasks", name="tasks")
+     */
+    public function showTasks(ManagerRegistry $doctrine): Response
+    {
+        $tasks = $doctrine->getRepository(Task::class)->findAll();
+
+        return $this->render('task/index.html.twig', array(
+            'tasks' => $tasks,
+        ));
+    }
+
+    /**
+     * @Route("/new_task", name="new_task")
+     */
+    public function new(ManagerRegistry $doctrine, Request $request): Response
+    {
+
+        $task = new Task();
+        /* $task->setName('Write a blog post');
+        $task->setDueDate(new \DateTime('tomorrow'));
+        $task->setType('PDF'); */
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $task = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('tasks');
+        }
+
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/edit_task/{id}", name="edit_task")
+     */
+    public function edit(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $task = $doctrine->getRepository(Task::class)->find($id);
+
+        $task->setName($task->getName());
+        $task->setDueDate($task->getDueDate());
+        $task->setType($task->getType());
+
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $task = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('tasks');
+        }
+
+        return $this->renderForm('task/new.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/remove_task/{id}", name="remove_task")
+     */
+    public function removeTask(ManagerRegistry $doctrine, int $id): Response
+    {
+        $task = $doctrine->getRepository(Task::class)->find($id);
+
+        var_dump($task);
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('tasks');
+    }
+}
---
+++ src/Entity/Task.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $dueDate;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $type;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDueDate(): ?\DateTimeInterface
+    {
+        return $this->dueDate;
+    }
+
+    public function setDueDate(\DateTimeInterface $dueDate): self
+    {
+        $this->dueDate = $dueDate;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+}
